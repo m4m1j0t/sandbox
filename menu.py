55495e6c446f2c7844fa938792baf95d78ed93e3
m4m1j0t <akimovviolist@gmail.com>
2024-10-21 18:02:37 +0700

    in process again

--- menu.py
+++ menu.py
@@ -1,18 +1,15 @@
 menu = ['Манная', 'Гречневая', 'Пшённая', 'Овсяная', 'Рисовая']
-result = []
+numbers = []
 number = int(input())
 if number <= 5:
-    numbers = list(range(number))
-    result += numbers
+    numbers += list(range(number))
 else:
     while number != 0:
         if number >= 5:
-            numbers = list(range(5))
+            numbers += list(range(5))
             number -= 5
-            result += numbers
         else:
-            numbers = list(range(number))
-            result += numbers
+            numbers += list(range(number))
             break
 for i in result:
     print(menu[i])
