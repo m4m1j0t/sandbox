3d8cf03870218e8466ff834ef4630e31a08b784a
m4m1j0t <akimovviolist@gmail.com>
2024-11-03 13:05:02 +0700

    Иногда выводит лишний символ

--- news2.0.py
+++ news2.0.py
@@ -1,15 +1,16 @@
 x = int(input())
 header_length = x
 header = ""
-for _ in range(0, n := int(input())):
+for _ in range(0, int(input())):
     string = input()
     if len(string) <= x:
-        header += f"{string} \n"
+        header += f"{string}\n"
         x -= len(string)
         header_length += 1
     else:
-        header += f"{string} \n"
+        header += f"{string}\n"
+        x = 0
 if header_length == len(header) or header_length > len(header):
     print(header)
 else:
-    print(f'{header[:header_length - 3]}...')
+    print(f'{header[:header_length - 3]}... {header_length}')
