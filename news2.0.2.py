7ed7578facb2fc2c60ecba767b9b7e287a60dba9
m4m1j0t <akimovviolist@gmail.com>
2024-11-18 13:02:21 +0700

    Выдаёт ожидаемый ответ.

--- news2.0.2.py
+++ news2.0.2.py
@@ -1,10 +1,12 @@
 size, line = int(input()), []
 for _ in range(int(input())):
     line.append(input())
-    for i in line:
-        if len(i) < size - 3:
-            print(i)
-        elif len(i) == size:
-            print(i)
-        elif len(i) > size - 3:
-            print(i[:size - 3] + '...')
+for i in line:
+    if size <= 3:
+        pass
+    elif len(i) < size - 3 or len(i) == size:
+        print(i)
+        size -= len(i)
+    elif len(i) > size - 3:
+        print(i[:size - 3] + '...')
+        size -= len(i)
