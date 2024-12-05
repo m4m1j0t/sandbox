8f8f5e31567226307502d6a5db4a1e060c29b541
m4m1j0t <akimovviolist@gmail.com>
2024-12-05 16:05:18 +0700

    again

--- Polish calculator.py
+++ Polish calculator.py
@@ -1,21 +1,22 @@
-numbers, result = [], ''
+numbers, operations, result = [], [], ''
 for i in input().split( ):
-    if i in ['+', '-', '*'] and len(numbers) >= 2:
+    if i in ['+', '-', '*']:
+        operations.append(i)
+    else:
+        numbers.append(i)
+    for j in operations:
         x, y = int(numbers[-2]), int(numbers[-1])
         if i == '+':
             result = x + y
         elif i == '-':
             result = x - y
-        else:
+        elif i == '*':
             result = x * y
+        else:
+            print('Что-то пошло не так...')
         if len(numbers) > 2:
             numbers = numbers[:-2]
             numbers.append(str(result))
         else:
-            numbers = numbers[:-2]
-            numbers.append(result)
-    elif i not in ['+', '-', '*']:
-        numbers.append(i)
-    else:
-        print('Типо ого...')
-print(result)
+            print(result)
+            break
