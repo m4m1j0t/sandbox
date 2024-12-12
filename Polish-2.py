057b16e04a907ef2332c45904fd698b542c17e81
m4m1j0t <akimovviolist@gmail.com>
2024-12-12 14:47:50 +0700

    Работает

---
+++ Polish-2.py
@@ -0,0 +1,36 @@
+numbers, result = [], ''
+for i in input().split():
+    if i in ['+', '-', '*']:
+        x, y = int(numbers[-2]), int(numbers[-1])
+        if i == '+':
+            result = x + y
+        elif i == '-':
+            result = x - y
+        else:
+            result = x * y
+        numbers = numbers[:-2]
+        numbers.append(result)
+    elif i in ['~', '!']:
+        if i == '~':
+            result = -numbers[0]
+            numbers = numbers[:-1]
+            numbers.append(result)
+        else:
+            a = 1
+            for b in range(1, numbers + 1):
+                a *= b
+            numbers = numbers[:-1]
+            numbers.append(a)
+    elif i in ['#', '@']:
+        if i == '#':
+            x = numbers[-1]
+            numbers.append(x)
+        else:
+            x, y, z = numbers[-1], numbers[-2], numbers[-3]
+            numbers = numbers[:-3]
+            numbers.append(x)
+            numbers.append(y)
+            numbers.append(z)
+    else:
+        numbers.append(i)
+print(numbers)
